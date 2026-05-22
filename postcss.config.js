bbc9334bc05428ea3d1f4700faf91046033947c3
thehive2026 <ash@dispatchhive.org>
2026-05-21 21:04:27 -0400

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+
+# Generate postcss.config.js
+
+postcss_config = """module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+"""
+
+print("=" * 60)
+print("FILE 38: postcss.config.js")
+print("=" * 60)
+print(postcss_config)
