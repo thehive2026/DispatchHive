dbd8700f80509051c42eb5d9719b33179c8c627e
thehive2026 <ash@dispatchhive.org>
2026-05-21 21:10:51 -0400

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,1 +1,17 @@
 
+# Generate lib/prisma.ts - Database client
+
+prisma_client = """import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+"""
+
+print("=" * 60)
+print("FILE 6: lib/prisma.ts")
+print("=" * 60)
+print(prisma_client)
+
