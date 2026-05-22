aa0a635f46c7ea62924011671bf578448ab2fd0a
thehive2026 <ash@dispatchhive.org>
2026-05-21 21:19:37 -0400

    Update providers.tsx

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,1 +1,17 @@
 
+# Generate components/providers.tsx - Session Provider Wrapper
+
+providers_tsx = """"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
+"""
+
+print("=" * 60)
+print("FILE 19: components/providers.tsx")
+print("=" * 60)
+print(providers_tsx)
+
