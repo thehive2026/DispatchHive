8170d4a0e3ac2d9fe5e0708b9b667e73ea08a8fa
thehive2026 <ash@dispatchhive.org>
2026-05-21 21:05:02 -0400

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,14 @@
+
+# Generate next-env.d.ts
+
+next_env = """/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+"""
+
+print("=" * 60)
+print("FILE 39: next-env.d.ts")
+print("=" * 60)
+print(next_env)
